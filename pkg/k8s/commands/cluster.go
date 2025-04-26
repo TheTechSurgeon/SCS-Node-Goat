f0499785d4c202ad038c20e3c433a435c47d5ee8
Benjamin Simpson <benjamin.simpson@checkmarx.com>
2025-04-26 18:38:55 -0400

    fresh install

---
+++ pkg/k8s/commands/cluster.go
@@ -0,0 +1,55 @@
+package commands
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"golang.org/x/exp/slices"
+	"golang.org/x/xerrors"
+
+	k8sArtifacts "github.com/aquasecurity/trivy-kubernetes/pkg/artifacts"
+	"github.com/aquasecurity/trivy-kubernetes/pkg/k8s"
+	"github.com/aquasecurity/trivy-kubernetes/pkg/trivyk8s"
+	"github.com/aquasecurity/trivy/pkg/flag"
+	"github.com/aquasecurity/trivy/pkg/types"
+)
+
+// clusterRun runs scan on kubernetes cluster
+func clusterRun(ctx context.Context, opts flag.Options, cluster k8s.Cluster) error {
+	// TODO: replace with log.Logger
+	logger, _ := zap.NewProduction()
+
+	if err := validateReportArguments(opts); err != nil {
+		return err
+	}
+	var artifacts []*k8sArtifacts.Artifact
+	var err error
+	switch opts.Format {
+	case types.FormatCycloneDX:
+		artifacts, err = trivyk8s.New(cluster, logger.Sugar()).ListClusterBomInfo(ctx)
+		if err != nil {
+			return xerrors.Errorf("get k8s artifacts with node info error: %w", err)
+		}
+	case types.FormatJSON, types.FormatTable:
+		if opts.Scanners.AnyEnabled(types.MisconfigScanner) && slices.Contains(opts.Components, "infra") {
+			artifacts, err = trivyk8s.New(cluster, logger.Sugar(), trivyk8s.WithExcludeOwned(opts.ExcludeOwned)).ListArtifactAndNodeInfo(ctx,
+				trivyk8s.WithScanJobNamespace(opts.NodeCollectorNamespace),
+				trivyk8s.WithIgnoreLabels(opts.ExcludeNodes),
+				trivyk8s.WithScanJobImageRef(opts.NodeCollectorImageRef),
+				trivyk8s.WithTolerations(opts.Tolerations))
+			if err != nil {
+				return xerrors.Errorf("get k8s artifacts with node info error: %w", err)
+			}
+		} else {
+			artifacts, err = trivyk8s.New(cluster, logger.Sugar()).ListArtifacts(ctx)
+			if err != nil {
+				return xerrors.Errorf("get k8s artifacts error: %w", err)
+			}
+		}
+	default:
+		return xerrors.Errorf(`unknown format %q. Use "json" or "table" or "cyclonedx"`, opts.Format)
+	}
+
+	runner := newRunner(opts, cluster.GetCurrentContext())
+	return runner.run(ctx, artifacts)
+}
